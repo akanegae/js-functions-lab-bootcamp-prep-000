fa01daf8f75f7172c858253575d8dc4df76ced7c
Alison Kanegae <alisonkanegae@gmail.com>
2018-08-20 13:26:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// write your code below!
+//expect (happy holidays) to equal (happy holidays)
+//expect(happyHolidaysTo('you')).toEqual('Happy holidays, you!')
+//expect(happyHolidayTo('Independence Day', 'you')).toEqual('Happy Independence Day, you!')
+//expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!")
+
+function happyHolidays(string) {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  console.log(`Happy Holidays, ${name}!`)
+  happyHolidaysTo('you')
+  return `Happy Holidays, ${name}!`
+}
+
+function happyHolidaysTo(holiday, name) {
+  console.log(`Happy ${holiday}, ${name}!`)
+  happyHolidaysTo(`Independence Day`, `you`)
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, days) {
+  console.log("Mothers Day", 'you')
+  return `It\'s ${days} days until ${holiday}!`
+}
